db4c1f01cadc1e0eae4f707300fb361a9ac2ccad
Darimus <bruno.passos@outlook.com.br>
2020-11-17 14:56:01 -0300

    :memo: Sistema de Autenticacao

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { Cliente } from "./Cliente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
 import { ContaCorrente } from "./Conta/ContaCorrente.js";
 import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 //Cliente 1
 const cliente1 = new Cliente('Ricardo', 11122233300);
@@ -15,6 +18,12 @@ const contaPoupancaRicardo = new ContaPoupanca(cliente1, 1001, 0)
 contaPoupancaRicardo.depositar(50);
 contaPoupancaRicardo.sacar(5);
 
+
+//Funcionarios
+const gerenteBruno = new Gerente ('Bruno', 7000, 11110000000);
+const diretorBruno = new Diretor ('Passos', 20000, 12345678900);
+
+const estaLogado = SistemaAutenticacao.login(diretor, 123);
+
 //Logs
-console.log(contaCorrenteRicardo);
-console.log(contaPoupancaRicardo);
+console.log(estaLogado);
